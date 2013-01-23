0af46fbd4cff5a9edad750b6938a120b991a1e88
Madhavi Padimi <padimi@Madhus-iMac.local>
2013-01-23 14:37:01 +0530

    index deleted

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,55 +1,65 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-
-
+describe "Static pages" do
 
   describe "Home page" do
 
-  	it "should have the content 'Sample App' " do
+    it "should have the h1 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
+    end
 
-    	visit '/static_pages/home'
-      page.should have_selector('h1', text: "Sample App")
-      
-  	end
+    it "should have the base title" do
+      visit root_path
+      page.should have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App")
+    end
 
-    #it "should have the h1 content" do
+    it "should not have a custom page title" do
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
+    end
+  end
 
-     # visit '/static_pages/home'
-     # page.should have_content('h1')
+  describe "Help page" do
 
-   # end
+    it "should have the h1 'Help'" do
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
+    end
 
+    it "should have the title 'Help'" do
+      visit help_path
+      page.should have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit about_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
 
   describe "Contact page" do
 
-      it "should have the h1 content" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
 
-        visit '/static_pages/contact'
-        page.should have_selector('h1', text:"Contact page")
-      end
-
-  end
-
-
-
-describe "Help page" do
-
-    it "should have the content 'Help'" do
-      
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
-
-
-
-  describe "About page" do
-
-    it "should have the content 'About Us'" do
-      
-    end
-  end
-
-
-
 end
